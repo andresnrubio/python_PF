cca4745071fe22ec39ebb8116a86dea90b0beeca
Andres Rubio <rubioandres1@gmail.com>
2023-12-07 00:51:45 -0300

    Se agrego importacion desde app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from modules.classes.py import Client
-
+from modules.classes import Client
 
 cliente_1 = Client("Andres", "Rubio", 123456, 11544564631, "andres@Rubio.com")
 
 print(cliente_1)
 print(cliente_1.get_interest())
+
+# import sys
+
+# print(f"{sys.argv}")
