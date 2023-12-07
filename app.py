1a828470ea379f334e5cf25892ebdccf97791dff
Andres Rubio <rubioandres1@gmail.com>
2023-12-07 00:35:18 -0300

    Se agrego carpeta modulo y app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from modules.classes.py import Client
+
+
+cliente_1 = Client("Andres", "Rubio", 123456, 11544564631, "andres@Rubio.com")
+
+print(cliente_1)
+print(cliente_1.get_interest())
--- modules/classes.py
+++ modules/classes.py
@@ -1,19 +1,24 @@
-class Cliente:
+class Client:
     def __init__(self, name, lastname, personalId, phone, emailAddress):
         self.name = name
         self.lastname = lastname
         self.__personalId = personalId
         self.__phone = phone
         self.__emailAddress = emailAddress
+        self.interest = ["movies", "series", "games"]
 
     def __str__(self):
         return f"El cliente es {self.name} {self.lastname}, con identificacion: {self.__personalId}"
 
-    def contact_Info(self):
+    def contact_info(self):
         return f"Contacto: {self.__phone}, {self.__emailAddress}"
 
+    def get_interest(self):
+        # Ver lo que indico el profesor para reformular los metodos de listas
+        return f"{self.interest}"
 
-cliente_1 = Cliente("Andres", "Rubio", 123456, 11544564631, "andres@Rubio.com")
+    def add_interest(self, new_interest):
+        pass
 
-print(cliente_1)
-print(cliente_1.contact_Info())
+    def delete_interest(self, interest_to_delete):
+        pass
